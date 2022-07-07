eb59d18b169d19b0937c0fed8ef9a66df37294a2
oswaldo orellana vasquez <oswaldo.orellana.v@gmail.com>
2022-07-07 19:30:08 -0400

    cambiar ruta

--- resources/views/layout/parts/wrapper.blade.php
+++ resources/views/layout/parts/wrapper.blade.php
@@ -20,39 +20,12 @@
     <!--begin::Toolbar wrapper-->
     <div class="d-flex align-items-stretch flex-shrink-0">
 
-       {{-- <!--begin::Chat-->
-        <div class="d-flex align-items-center ms-1 ms-lg-3">
-            <!--begin::Menu wrapper-->
-            <div class="btn btn-icon btn-icon-muted btn-active-light btn-active-color-primary w-30px h-30px w-md-40px h-md-40px position-relative"
-                id="kt_drawer_chat_toggle">
-                <!--begin::Svg Icon | path: icons/duotune/communication/com012.svg-->
-                <span class="svg-icon svg-icon-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        fill="none">
-                        <path opacity="0.3"
-                            d="M20 3H4C2.89543 3 2 3.89543 2 5V16C2 17.1046 2.89543 18 4 18H4.5C5.05228 18 5.5 18.4477 5.5 19V21.5052C5.5 22.1441 6.21212 22.5253 6.74376 22.1708L11.4885 19.0077C12.4741 18.3506 13.6321 18 14.8167 18H20C21.1046 18 22 17.1046 22 16V5C22 3.89543 21.1046 3 20 3Z"
-                            fill="black" />
-                        <rect x="6" y="12" width="7" height="2" rx="1"
-                            fill="black" />
-                        <rect x="6" y="7" width="12" height="2" rx="1"
-                            fill="black" />
-                    </svg>
-                </span>
-                <!--end::Svg Icon-->
-                <span
-                    class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
-            </div>
-            <!--end::Menu wrapper-->
-        </div>
-        <!--end::Chat-->--}}
-
-
         <!--begin::User menu-->
         <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
             <!--begin::Menu wrapper-->
             <div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click"
                 data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                <img src="{{asset('assets/media/avatars/300-1.jpg')}}" alt="user" />
+                <img src="{{ asset('assets/media/avatars/300-1.jpg') }}" alt="user" />
             </div>
             <!--begin::User account menu-->
             <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px"
@@ -62,15 +35,17 @@
                     <div class="menu-content d-flex align-items-center px-3">
                         <!--begin::Avatar-->
                         <div class="symbol symbol-50px me-5">
-                            <img alt="Logo" src="{{asset('assets/media/avatars/300-1.jpg')}}" />
+                            <img alt="Logo" src="{{ asset('assets/media/avatars/300-1.jpg') }}" />
                         </div>
                         <!--end::Avatar-->
                         <!--begin::Username-->
                         <div class="d-flex flex-column">
-                            <div class="fw-bolder d-flex align-items-center fs-5 text-capitalize">{{Auth::user()->name }}
+                            <div class="fw-bolder d-flex align-items-center fs-5 text-capitalize">
+                                {{ Auth::user()->name }}
                                 <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2"></span>
                             </div>
-                            <a href="#" class="fw-bold text-muted text-hover-primary fs-7 text-capitalize">{{Auth::user()->last_name }}</a>
+                            <a href="#"
+                                class="fw-bold text-muted text-hover-primary fs-7 text-capitalize">{{ Auth::user()->last_name }}</a>
                         </div>
                         <!--end::Username-->
                     </div>
@@ -82,7 +57,7 @@
                 <!--begin::Menu item-->
                 <div class="menu-item px-5">
                     <a href="../../demo1/dist/account/overview.html" class="menu-link px-5">Mi perfil
-                        </a>
+                    </a>
                 </div>
                 <!--end::Menu item-->
 
@@ -95,50 +70,16 @@
 
                 <!--begin::Menu item-->
                 <div class="menu-item px-5">
-                    <form action="{{route('logout')}}" method="POST" id="logout">
+                    <a href="javascript:$('#logout').submit();" class="menu-link px-5">Cerrar sesión</a>
+                    <form action="{{ route('logout') }}" method="POST" id="logout">
                         @csrf
-                        <a href="javascript:$('#logout').submit();" class="menu-link px-5">Cerrar sesión</a>
                     </form>
                 </div>
-                <!--end::Menu item-->
-                <!--begin::Menu item-->
-                {{--<div class="menu-item px-5">
-                    <div class="menu-content px-5">
-                        <label class="form-check form-switch form-check-custom form-check-solid pulse pulse-success"
-                            for="kt_user_menu_dark_mode_toggle">
-                            <input class="form-check-input w-30px h-20px" type="checkbox" value="1" name="mode"
-                                id="kt_user_menu_dark_mode_toggle" data-kt-url="../../demo1/dist/index.html" />
-                            <span class="pulse-ring ms-n1"></span>
-                            <span class="form-check-label text-gray-600 fs-7">Dark Mode</span>
-                        </label>
-                    </div>
-                </div>--}}
-                <!--end::Menu item-->
             </div>
             <!--end::User account menu-->
             <!--end::Menu wrapper-->
         </div>
         <!--end::User menu-->
-        {{--<!--begin::Header menu toggle-->
-        <div class="d-flex align-items-center d-lg-none ms-2 me-n3" title="Show header menu">
-            <div class="btn btn-icon btn-active-light-primary w-30px h-30px w-md-40px h-md-40px"
-                id="kt_header_menu_mobile_toggle">
-                <!--begin::Svg Icon | path: icons/duotune/text/txt001.svg-->
-                <span class="svg-icon svg-icon-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        fill="none">
-                        <path
-                            d="M13 11H3C2.4 11 2 10.6 2 10V9C2 8.4 2.4 8 3 8H13C13.6 8 14 8.4 14 9V10C14 10.6 13.6 11 13 11ZM22 5V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4V5C2 5.6 2.4 6 3 6H21C21.6 6 22 5.6 22 5Z"
-                            fill="black" />
-                        <path opacity="0.3"
-                            d="M21 16H3C2.4 16 2 15.6 2 15V14C2 13.4 2.4 13 3 13H21C21.6 13 22 13.4 22 14V15C22 15.6 21.6 16 21 16ZM14 20V19C14 18.4 13.6 18 13 18H3C2.4 18 2 18.4 2 19V20C2 20.6 2.4 21 3 21H13C13.6 21 14 20.6 14 20Z"
-                            fill="black" />
-                    </svg>
-                </span>
-                <!--end::Svg Icon-->
-            </div>
-        </div>
-        <!--end::Header menu toggle-->--}}
     </div>
     <!--end::Toolbar wrapper-->
 </div>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(["auth"])->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 
     Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
