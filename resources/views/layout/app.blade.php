871fe46a147a1750e8df15adc6b0e6d805dbc224
oswaldo orellana vasquez <oswaldo.orellana.v@gmail.com>
2022-09-14 23:30:39 -0400

    meta imagenes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,15 +1,4 @@
 <!DOCTYPE html>
-<!--
-Author: Keenthemes
-Product Name: Metronic - Bootstrap 5 HTML, VueJS, React, Angular & Laravel Admin Dashboard Theme
-Purchase: https://1.envato.market/EA4JP
-Website: http://www.keenthemes.com
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
 <html lang="en">
 <!--begin::Head-->
 
@@ -17,17 +6,21 @@ License: For each use you must have a valid license purchased only from above li
     <base href="../../">
     <title>Iglesia del Dios vivo</title>
     <meta charset="utf-8" />
-    <meta name="description"
+    <!--<meta name="description"
         content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
     <meta name="keywords"
         content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    -->
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title"
-        content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular &amp; Laravel Admin Dashboard Theme" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Keenthemes | Metronic" />
+    <meta property="og:url" content="https://iglesia-diosvivo.com/admin/login" />
+    <meta property="og:site_name" content="Iglesia del Dios vivo">
+    <meta property="og:title" content="Iglesia del Dios vivo" />
+    <meta property="og:description" content="Iglesia evangelica del Dios vivo" />
+    <meta property="og:image" itemprop="image" content="{{ asset('assets/media/dios-vivo-logo.png') }}">
+    <meta property="og:type" content="website" />
+    <meta property="og:updated_time" content="1440432930" />
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('layout.parts.link')
     @stack('css')
