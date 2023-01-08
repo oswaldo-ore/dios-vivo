e64f8e58ef2d5359925daf820e0ef0d8594a72b5
oswaldo orellana vasquez <oswaldo.orellana.v@gmail.com>
2023-01-08 02:01:52 -0400

    a

--- resources/views/admin/business/index.blade.php
+++ resources/views/admin/business/index.blade.php
@@ -337,7 +337,7 @@
 
         $(document).ready(function() {
             $("#menu-business").addClass('active open');
-            if(business.show_report_public || business.show_report_yearly){
+            if(!business.show_report_public || !business.show_report_yearly){
                 $('.mostrar_reportes input').prop('disabled',true);
             }
         });
